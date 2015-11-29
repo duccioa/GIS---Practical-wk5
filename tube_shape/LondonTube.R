c4aeafa2d3fa24ba695b476ee227b647694004a7
duccioa <duccio.aiazzi@gmail.com>
2015-11-29 22:22:56 +0000

    Blue plaques added

--- tube_shape/LondonTube.R
+++ tube_shape/LondonTube.R
@@ -76,66 +76,83 @@ points(stations, pch = 20, cex = 0.5, col = "black")
 Central@lines[[3]]@Lines[[1]]@coords[1,2]
 ###FUNCTIONS###
 #Given a SpatialLinesDataFrame, calculate the length of the segment corresponding to the id
-seg_length <- function(line, id){
-    xx1 <- line@lines[[id]]@Lines[[1]]@coords[1,1]
+seg_length <- function(line, id){#input a line and the id corresponding to the selected segment
+    xx1 <- line@lines[[id]]@Lines[[1]]@coords[1,1]#get the coords of the two points on the segment
     xx2 <- line@lines[[id]]@Lines[[1]]@coords[2,1]
     yy1 <- line@lines[[id]]@Lines[[1]]@coords[1,2]
     yy2 <- line@lines[[id]]@Lines[[1]]@coords[2,2]
-    x1 <- min(xx1, xx2)
+    x1 <- min(xx1, xx2)#put them in order
     x2 <- max(xx1, xx2)
     y1 <- min(yy1, yy2)
     y2 <- max(yy1, yy2)
-    len <- sqrt((x2-x1)^2+(y2-y1)^2)
+    len <- sqrt((x2-x1)^2+(y2-y1)^2)#calculate with pitagora the length of the segment
     return(len)
 }
 #Given a patialLinesDataFrame, calculate the total length
 line_length <- function(line){
-    num_seg <- length(line@lines)
+    num_seg <- length(line@lines)#how many segments in the line?
     total_length <- 0
-    for(i in 1:num_seg){
+    for(i in 1:num_seg){#go through each segment, calculate the length and add it up
         total_length <- total_length + seg_length(line, i)
     }
     return(total_length)
 }
 
 ####PRACTICE 5
-##question 1 - Whic is the longest tube line in the dataset?
-
+##QUESTION 1 - Which is the longest tube line in the dataset?
+#Takes a list of SpatialLinesDataFrame already loaded and the rank required, returns the name and the 
+#length of the line corresponding to the rank
 which_length <- function(list, rank){
-    x <- data.frame(Line = list, Length = rep(0, length(list)), stringsAsFactors = FALSE)
-    for(i in 1:length(list)){
-        x[i, 2] <- line_length(get(list[i]))
+    x <- data.frame(Line = list, Length = rep(0, length(list)), stringsAsFactors = FALSE)#dataframe with line name and length
+    for(i in 1:length(list)){#get the name from the first column and feed it to the function line_length
+        x[i, 2] <- line_length(get(list[i]))#in order to return the length and place it in the dataframe
     }
     x <- x[order(-x$Length),]
     return(data.frame(LongestLine = x[rank,1], Length = x[rank,2], Units = "m"))
 }
 which_length(line_list, 1)
 
-##Question 2 - How many stations on the Piccadilly
-Piccadilly_buffer <- gBuffer(Piccadilly, width = 0.0001)
-Piccadilly_stations <- stations[Piccadilly_buffer, ]
+##QUESTION 2 - How many stations on the Piccadilly
+Piccadilly_buffer <- gBuffer(Piccadilly, width = 0.1)#create a buffer zone around the line
+Piccadilly_stations <- stations[Piccadilly_buffer, ]#intersect with the stations
 summary(Piccadilly_stations)
 plot(Piccadilly_buffer)
 points(Piccadilly_stations, col = "red")
-##Question 3 - Average price along the wards of each line and which is the highest
-download.file(url = "https://files.datapress.com/london/dataset/average-house-prices-ward-lsoa-msoa/house-prices-Wards.csv",
-              dest = "./house-prices-wards.csv", method = "curl")
+##QUESTION 3 - Average price along the wards of each line and which is the highest
+#download.file(url = "https://files.datapress.com/london/dataset/average-house-prices-ward-lsoa-msoa/house-prices-Wards.csv",
+              #dest = "./house-prices-wards.csv", method = "curl")
 #replace in excel "(£)" with "_GBP_" and "-" with ""
 house_prices_wards <- read.csv("./house-prices-wards.csv", sep = ",", stringsAsFactors = FALSE)
 names(house_prices_wards)[1] <- "GSS_CODE"
-wards@data <- join(wards@data, house_prices_wards)
-wards_Central <- wards[Central,]
-mean(wards_Central@data$Mean_GBP_2014, na.rm = TRUE)
+wards@data <- join(wards@data, house_prices_wards)#joint geometric data and census
+#The function takes the list of tube lines and return the average house price 2014
 avg_houseprice_along <- function(List){
-    avg_price <- data.frame(Line = List$Line_Name, avg_GBP_HousePrice2014 = 0, stringsAsFactors = FALSE)
+    avg_price <- data.frame(Line = List, avg_GBP_HousePrice2014 = 0, stringsAsFactors = FALSE)#create the result dataframe
     for(i in 1:length(avg_price$Line)){
-        name <- avg_price[i,1]
-        wards_name <- wards[get(name),]
-        avg_price[i,2] <- mean(wards_name@data$Mean_GBP_2014, na.rm = TRUE)
+        name <- avg_price[i,1]#take the name of the line
+        wards_name <- wards[get(name),]#intersect the line and the wards
+        avg_price[i,2] <- mean(wards_name@data$Mean_GBP_2014, na.rm = TRUE)#caclulate avg price
     }
+    avg_price <- avg_price[order(-avg_price$avg_GBP_HousePrice2014),]
     return(avg_price)
 }
-avg_houseprice_along(tube_lines)
+avg_houseprice_along(line_list)
 
-#
+##QUESTION 4
+BluePlaques <- readOGR("./LondonBluePlaques/", "LondonBluePlaques")
+proj4string(BluePlaques) <- CRS("+init=epsg:4326")#Assign correct name for wgs84
+BluePlaques <- spTransform(BluePlaques, CRS("+init=epsg:27700"))#transform to British National Grid
+plot(wards)
+points(BluePlaques, col = "blue", cex = .1, pch =20)
+#Blue plaques around the Jubilee line
+Jubilee_buffer <- gBuffer(Jubilee, width = 200)
+BluePlaques_Jub <- BluePlaques[Jubilee_buffer,]
+plot(wards)
+plot(Jubilee_buffer, col = "red", cex = .1, add = TRUE)
+points(BluePlaques_Jub, col = "blue", cex = .1, pch =20)
+summary(BluePlaques_Jub)
+
+Jubilee_station <- stations[stations@data$Name == ""]
+
+
 
